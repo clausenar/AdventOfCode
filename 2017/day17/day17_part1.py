3d6522fc3348b09c0cbed4a3bd8407782bf38969
Anders R. Clausen <anders.clausen@gu.se>
2022-08-26 23:15:04 +0200

    Update day17_part1.py

--- 2017/day17/day17_part1.py
+++ 2017/day17/day17_part1.py
@@ -6,7 +6,4 @@ def spinlock(skip):
         insertion=(current+skip)%len(l)+1
         l.insert(insertion,i)
         current=insertion
-
-    for n,value in enumerate(l):
-        if value==2017:
-            return (l[n+1])
+    return (l[current+1])
