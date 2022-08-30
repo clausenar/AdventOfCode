127a98923eb409cee3e011041bbb03d8572d6874
Anders R. Clausen <anders.clausen@gu.se>
2022-08-30 17:33:49 +0200

    Create part1_part2.py

---
+++ 2017/day20/part1_part2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def load_data():
+    data=pd.read_csv("input_20",sep=",",header=None)
+    data=data.replace("p=<","",regex=True).replace("v=<","",regex=True).replace("a=<","",regex=True).replace(">","",regex=True)
+    data=data.astype(int)
+    return data
+
+def find_closest():
+    data=load_data()
+
+    for i in range(400):
+        data[5]+=data[8]
+        data[4]+=data[7]
+        data[3]+=data[6]
+        data[2]+=data[5]
+        data[1]+=data[4]
+        data[0]+=data[3]
+
+        data['sum']=abs(data[0])+abs(data[1])+abs(data[2])
+
+    particle= (data[data['sum']==data['sum'].min()].index.values.astype(int)[0])
+    print ("The closest particle is:",particle)
+
+def find_closest_collision():
+
+    data=load_data()
+
+    for i in range(400):
+        data.drop_duplicates(subset=[0,1,2],keep=False,inplace=True)
+        data[5]+=data[8]
+        data[4]+=data[7]
+        data[3]+=data[6]
+        data[2]+=data[5]
+        data[1]+=data[4]
+        data[0]+=data[3]
+        data['sum']=abs(data[0])+abs(data[1])+abs(data[2])
+
+    print ("The remaining number of particles is:",data.shape[0])   
