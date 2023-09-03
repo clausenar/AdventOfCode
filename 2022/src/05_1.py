977c988df1095c4c49c2209b6768c8e0d95cbc86
Anders Clausen <anders.r.clausen@gmail.com>
2023-09-04 00:34:48 +0200

    day 5, part 1

---
+++ 2022/src/05_1.py
@@ -0,0 +1,31 @@
+f = open("./data/05_input.txt")
+
+start = f.readlines()
+stacks = int(len(start)/3)
+all_stacks = [[] for i in range(stacks) ]
+
+for i in start:
+    i=i.strip("[").strip("]")
+    for key,value in enumerate(i):
+        print (key,value)
+        if value.isalpha() and value.isupper():
+            all_stacks[key//4].append(value)
+
+for i in start:
+    if i.islower():
+        print ("here comes instructions",i)
+        _,number,_,from_,_,to_=i.split(" ")
+        for r in range(int(number)):
+            package = all_stacks[int(from_)-1].pop(0)
+            all_stacks[int(to_)-1].insert(0,package)
+             
+new_string = []
+
+for i in range(len(all_stacks)-1):
+    if len(all_stacks[i])>0:
+        new_string.append(all_stacks[i][0])
+    
+print ("Answer to first part:","".join(new_string))
+
+        
+        
