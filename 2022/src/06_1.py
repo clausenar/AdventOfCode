feee5969e6d74ec0d88e2843cff6a7f568d2439b
Anders Clausen <anders.r.clausen@gmail.com>
2023-09-05 22:19:32 +0200

    day 6 part 2

--- 2022/src/06_1.py
+++ 2022/src/06_1.py
@@ -1,9 +1,14 @@
 file = open("./data/06_input.txt")
-
 seq = file.read()
 
 for i in range(len(seq)-3):
     block = seq[i:i+4]
     if len(set(block))==4:
         print ("Marker will start at:",i+4)
+        break
+
+for i in range(len(seq)-13):
+    block = seq[i:i+14]
+    if len(set(block))==14:
+        print ("Marker will start at:",i+14)
         break
