1a2549c8691d4137ccff7cad5fd30aa7f88fbb3f
Anders Clausen <anders.r.clausen@gmail.com>
2025-02-20 21:59:50 +0000

    Challenge 1

--- 2024/data/1_1.py
+++ 2024/data/1_1.py
@@ -1,8 +1,11 @@
+from collections import Counter
+
 list1=[]
 list2=[]
 length = 0
+similarity = 0
 
-with open("./1_test.txt") as f:
+with open("./1.txt") as f:
     for line in f:
         list1.append((line.split("   ")[0]))
         list2.append((line.split("   ")[1]).strip())
@@ -14,3 +17,12 @@ for i in range(len(list1)):
     length += abs(int(list1[i])-int(list2[i]))
 
 print ("Solution to problem #1 is:",length)
+
+items1= Counter(list1)
+items2= Counter(list2)
+
+for i in list1:    
+    similarity += int(i)*items2[(i)]
+
+print ("Solution to problem #2 is:",similarity)
+
